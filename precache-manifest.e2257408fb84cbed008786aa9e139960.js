0c6d96a3a44e3639ab521b98ec1a99e8952914dc
Valeriy-Sorochynskyi <haribol162@gmail.com>
2019-05-19 15:30:09 +0300

    Updates

---
+++ precache-manifest.e2257408fb84cbed008786aa9e139960.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "478d694035bf0636cbeeb0394a66eef4",
+    "url": "/Valeriy-Sorochynskyi/data-table-component/index.html"
+  },
+  {
+    "revision": "a6efa84c99bc56aa9388",
+    "url": "/Valeriy-Sorochynskyi/data-table-component/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/Valeriy-Sorochynskyi/data-table-component/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "a6efa84c99bc56aa9388",
+    "url": "/Valeriy-Sorochynskyi/data-table-component/static/js/main.eae88df1.chunk.js"
+  },
+  {
+    "revision": "5d4d96b0c00bbd7d41b3",
+    "url": "/Valeriy-Sorochynskyi/data-table-component/static/js/runtime~main.bfe6ab09.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Valeriy-Sorochynskyi/data-table-component/static/media/logo.5d5d9eef.svg"
+  }
+]);
